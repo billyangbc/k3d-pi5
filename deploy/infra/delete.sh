42527faba91acf8b4d6d1eee35c2e587a6fb7325
Bill Yang <bill.yang@live.ca>
2024-09-30 12:28:55 -0700

    updated scripts

--- deploy/infra/delete.sh
+++ deploy/infra/delete.sh
@@ -11,4 +11,9 @@ kubectl delete -f mongo-express.yaml
 kubectl delete -f mongo.yaml 
 kubectl delete -f nextcloud.yaml 
 kubectl delete -f postgres.yaml 
-kubectl delete -f redis.yaml 
+kubectl delete -f redis.yaml
+
+kubectl delete -f hello-app.yaml
+kubectl delete -f ingress.yaml
+
+kubectl delete -f ns.yaml
--- deploy/infra/deploy.sh
+++ deploy/infra/deploy.sh
@@ -10,9 +10,13 @@
 #
 # Don't forget to change kubectl to your production Kubernetes instance
 #
+kubectl apply -f ns.yaml
 kubectl apply -f mariadb.yaml
 kubectl apply -f mongo-express.yaml 
 kubectl apply -f mongo.yaml 
 kubectl apply -f nextcloud.yaml 
 kubectl apply -f postgres.yaml 
-kubectl apply -f redis.yaml 
+kubectl apply -f redis.yaml
+
+kubectl apply -f hello-app.yaml
+kubectl apply -f ingress.yaml
